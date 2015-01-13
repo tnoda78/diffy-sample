dccae0e518932534317780bc3fc314573a3f1b07
tnoda78 <tn78adsl@gmail.com>
2015-01-13 18:43:18 +0900

    Change diff heml format

--- app/models/diff.rb
+++ app/models/diff.rb
@@ -4,7 +4,8 @@ class Diff
   attr_accessor :before, :after
 
   def diff_content
-    Diffy::Diff.new(self.before, self.after, include_plus_and_minus_in_html: true)
-               .to_s(:html_simple)
+    diff_info = Diffy::Diff.new(self.before, self.after, diff: "-U 3", include_plus_and_minus_in_html: true,
+                               include_diff_info: true)
+    diff_info.to_s(:html)
   end
 end
