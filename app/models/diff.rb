e3914723865b58d980b03d56aadd8d231242b92b
tnoda78 <tn78adsl@gmail.com>
2015-02-02 21:05:55 +0900

    Update compare strings

--- app/models/diff.rb
+++ app/models/diff.rb
@@ -4,8 +4,17 @@ class Diff
   attr_accessor :before, :after
 
   def diff_content
-    diff_info = Diffy::Diff.new(self.before, self.after, diff: "-U 3", include_plus_and_minus_in_html: true,
+    diff_before = add_newline(self.before)
+    diff_after = add_newline(self.after)
+    diff_info = Diffy::Diff.new(diff_before, diff_after, diff: "-U 3", include_plus_and_minus_in_html: true,
                                include_diff_info: true)
     diff_info.to_s(:html)
+  end
+
+  private
+
+  def add_newline(str)
+    return "" unless str.present?
+    "#{str.chomp}#{str.scan(/\r\n|\n|\n/)[0] || "\r\n"}"
   end
 end
