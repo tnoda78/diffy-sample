bc862d0011cba1f05eeb4fecdbd74f31bb97c5c4
tnoda78 <tn78adsl@gmail.com>
2015-02-03 20:21:35 +0900

    Refactor model

--- app/models/diff.rb
+++ app/models/diff.rb
@@ -4,8 +4,6 @@ class Diff
   attr_accessor :before, :after
 
   def diff_content
-    diff_before = add_newline(self.before)
-    diff_after = add_newline(self.after)
     diff_info = Diffy::Diff.new(diff_before, diff_after, diff: "-U 3", include_plus_and_minus_in_html: true,
                                include_diff_info: true)
     diff_info.to_s(:html)
@@ -13,8 +11,15 @@ class Diff
 
   private
 
-  def add_newline(str)
-    return "" unless str.present?
-    "#{str.chomp}#{str.scan(/\r\n|\n|\n/)[0] || "\r\n"}"
+  def diff_before
+    "#{self.before.chomp}#{new_line}"
+  end
+
+  def diff_after
+    "#{self.after.chomp}#{new_line}"
+  end
+
+  def new_line
+    self.before.scan(/\r\n|\n|\n/)[0] || self.after.scan(/\r\n|\n|\n/)[0] || ""
   end
 end
